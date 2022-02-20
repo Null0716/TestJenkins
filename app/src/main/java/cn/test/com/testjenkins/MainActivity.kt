fe772823d3a7431c2613813dcac225e272dc082d
leijiang3 <LJIANG33@volvocars.com>
2022-02-20 21:21:24 +0800

    fix: fix detekt

--- app/src/main/java/cn/test/com/testjenkins/MainActivity.kt
+++ app/src/main/java/cn/test/com/testjenkins/MainActivity.kt
@@ -3,7 +3,6 @@ package cn.test.com.testjenkins
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import java.lang.String
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
