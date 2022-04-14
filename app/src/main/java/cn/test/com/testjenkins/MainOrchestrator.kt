b615d30ea9b61463ac1d23bec04f494dc0a8bc6a
leijiang3 <LJIANG33@volvocars.com>
2022-04-14 22:18:01 +0800

    feat: 修改策略

--- app/src/main/java/cn/test/com/testjenkins/MainOrchestrator.kt
+++ app/src/main/java/cn/test/com/testjenkins/MainOrchestrator.kt
@@ -2,9 +2,7 @@ package cn.test.com.testjenkins
 
 import cn.parch.com.Orchestrator
 
-interface MainOrchestrator: Orchestrator {
-
-}
+interface MainOrchestrator: Orchestrator
 
 class MainOrchestratorImpl():Orchestrator{
     override fun onStart() {
